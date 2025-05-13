f24306ec9145eafe686a2681f017c4ed313300ae
laila-chk <chokri.laila.ch@gmail.com>
2025-05-13 15:29:59 +0100

    norming

--- Starting/ex00/Hello.py
+++ Starting/ex00/Hello.py
@@ -1,10 +1,9 @@
 ft_list = ["Hello", "World!"]
 ft_tuple = ("Hello", "Morocco!")
 ft_set = {"Hello", "Benguerir!"}
-ft_dict = {"Hello" : "1337!"}
+ft_dict = {"Hello": "1337!"}
 
 print(ft_list)
 print(ft_tuple)
 print(ft_set)
 print(ft_dict)
-
