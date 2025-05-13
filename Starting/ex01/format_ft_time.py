a9f34679c5eb174d876c62a84232538d64bd8f30
laila-chk <chokri.laila.ch@gmail.com>
2025-05-13 17:31:22 +0100

    norming

--- Starting/ex01/format_ft_time.py
+++ Starting/ex01/format_ft_time.py
@@ -1,7 +1,7 @@
 import datetime
 
-seconds = (datetime.datetime.now() - datetime.datetime(1970, 1,
-                                       1)).total_seconds()
+timeNow = datetime.datetime.now() 
+seconds = (timeNow - datetime.datetime(1970, 1, 1)).total_seconds()
 print(f"Seconds since January 1, 1970: {seconds:,.4f} or "
       f"{seconds:.2e} in scientific notation")
 print(datetime.datetime.now().strftime("%b %d %Y"))
