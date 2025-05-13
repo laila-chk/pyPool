cd19f1ce3c0bcf09e751988e802a33076960fb4c
laila-chk <chokri.laila.ch@gmail.com>
2025-05-13 17:48:37 +0100

    norming

--- Starting/ex03/NULL_not_found.py
+++ Starting/ex03/NULL_not_found.py
@@ -5,11 +5,11 @@ def NULL_not_found(object: any) -> int:
         print("Cheese: ", object, type(object))
     elif not object:
         if isinstance(object, bool):
-            print("Fake: ",object, type(object))
+            print("Fake: ", object, type(object))
         elif isinstance(object, int):
             print("Zero: ", object, type(object))
         elif isinstance(object, str):
-            print("Empty: ",object, type(object))
+            print("Empty: ", object, type(object))
     else:
         print("Type not Found")
         return 1
