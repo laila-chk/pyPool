be678bee04a015cbd30e6ebac27a1bd31369cd02
laila-chk <chokri.laila.ch@gmail.com>
2025-11-16 21:29:52 +0100

    ex04

---
+++ Array/ex04/rotate.py
@@ -0,0 +1,20 @@
+from load_image import ft_load
+
+def rotate():
+    """
+    this function loads an image, prints its format, and its pixels
+    content in RGB format.
+    then it crops the image to 400x400 px, grayscales it, prints again the new values
+    then renders the new image.
+    """
+    try:
+        ft_load("animal.jpeg", False)
+        ft_load("animal.jpeg", True)
+    except Exception :
+        exit()
+
+def main():
+    rotate()
+
+if __name__ == "__main__":
+    main()
