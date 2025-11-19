cda5b7c39589a118fcf81f41b5b050ea7bf55d17
laila-chk <chokri.laila.ch@gmail.com>
2025-11-19 15:47:16 +0100

    norming

--- Array/ex00/give_bmi.py
+++ Array/ex00/give_bmi.py
@@ -1,4 +1,5 @@
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
     """calculates BMI values for two passed lists
         arg 1: list of heights (ints or floats)
         arg 2: list of wes (ints or floats)
@@ -9,33 +10,22 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
         exit("Error! lengths of passed arguments should be the same.")
     bmi = []
     for h, w in zip(height, weight):
-        if((type(h) is not int and type(h) is not float) or (type(w) is not int and type(w) is not float)):
+        if ((type(h) is not int and type(h) is not float
+             ) or (type(w) is not int and type(w) is not float)):
             exit("Error! Arrays should contain ints and floats only.")
         bmi.append(w/(h*h))
     return bmi
 
 
-
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    """returns a list of booleans True if list element the is above limit, othrewise False"""
+    """returns a list of booleans True if list element the is above limit,
+    othrewise False"""
     lim = []
     for val in bmi:
-        if(type(val) is not int and type(val) is not float):
+        if (type(val) is not int and type(val) is not float):
             exit("Error! Array should contain ints and floats only.")
         if (val > limit):
             lim.append(True)
         else:
             lim.append(False)
     return lim
-
-# def main():
-#     height = [2.71, 1.15]
-#     weight = [165.3, 38.4]
-#     bmi = give_bmi(height, weight)
-#     print(bmi, type(bmi))
-#     print(apply_limit(height, 26))
-
-
-
-# if __name__ == "__main__":
-#     main()
--- Array/ex02/load_image.py
+++ Array/ex02/load_image.py
@@ -2,21 +2,16 @@ from PIL import Image
 import numpy as np
 from numpy._typing import NDArray
 
+
 def ft_load(path: str) -> NDArray:
     """this function loads an image, prints its format, and its pixels
     content and RGB format. """
     try:
         img = Image.open(path)
-    except Exception :
-        exit("Error while trying to read the image! make sure the path is correct.")
+    except Exception:
+        exit("Error reading the image! make sure the path is correct.")
     data = np.asarray(img)
-    print(f"The shape of image is: ({img.size[1]}, {img.size[0]}, {len(img.mode)})")
-    print(f":{data}")
+    print(f"The shape of image is: ({img.size[1]}, "
+        f"{img.size[0]}, {len(img.mode)})")
+    print(f"{data}")
     return data
-
-import sys
-def main():
-    ft_load(sys.argv[1])
-
-if __name__ == "__main__":
-    main()
